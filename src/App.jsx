f7cf82832ce23f34f63e62909b56de4c070802c5
DanielVarjaskéri <varjaskerid@gmail.com>
2023-01-30 16:29:18 +0100

    answeres passed down, randomised

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import Question from "./Question.jsx";
+import { nanoid } from 'nanoid';
 
 
 
 function App() {
   const [gameStarted,setGameStarted] = React.useState(true); //show or hide start page
+  const [questions,setQuestions] = React.useState([]);
+  console.log(questions);
   
 
   React.useEffect(              /////load questions
     ()=>{
       console.log('effect ran');
-      fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+      fetch("https://opentdb.com/api.php?amount=1&type=multiple")
         .then(resp=>resp.json())
-        .then(questions => console.log(questions));
+        .then(questions => setQuestions(questions.results.map(item=>{
+         return {qu: item.question,
+                correctAnswer : item.correct_answer,
+                incorrect_answers: item.incorrect_answers}  })))
+        
     },[]
   );
 
+
+  const questionsToRender = questions.map(
+    question => {
+      return <Question 
+              key={nanoid()} 
+              question={question.qu} 
+              correctAnswer={question.correctAnswer}
+              incorrectAnswer={question.incorrect_answers}
+              />
+    }
+  );
 
 
   return (
@@ -29,7 +47,7 @@ function App() {
          </div>
         } 
 
-        <Question/>
+        {questionsToRender}
 
       </div> //app--container
      
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 
-function Question(){
+function Question(props){
+
+    const allAnswers = props.incorrectAnswer.map(item=>item);
+    allAnswers.push(props.correctAnswer);
+
+    
+    const shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        }
+    }
+    
+   
+    console.log(allAnswers);
+    shuffleArray(allAnswers);
+    console.log(allAnswers);
+
+
     return(
         <div className = "question--div">
-            <p>Question</p>
+            <p>{props.question}</p>
             <p>answers</p>
         </div>
     );
