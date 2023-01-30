575319e4f93f7001f893e243737f824cceb6002b
DanielVarjaskéri <varjaskerid@gmail.com>
2023-01-30 14:32:19 +0100

    start page set up n no style

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,22 @@ import React from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStarted,setGameStarted] = React.useState(false);
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div className='app--container'>
+
+        { !gameStarted && <div className="startPage--overlay">
+            <h1>Quizzical</h1>
+            <p>a Scrimba Reac Project</p>
+            <button>Start quiz</button>
+         </div>
+        } 
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     
   )
 }
 
